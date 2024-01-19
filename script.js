651715c76a36658731a2e7765c0d03734f69f438
JoannaAgnieszka <jkwasniowska@gmail.com>
2024-01-19 13:14:16 +0100

    mój pierwszy commit

---
+++ script.js
@@ -0,0 +1,512 @@
+const questionsGeography = [
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    correctAnswer: "Paris"
+  },
+  {
+    question: "What is the tallest mountain in the world?",
+    options: [
+      "Mount Kilimanjaro",
+      "Mount Everest",
+      "Mount Fuji",
+      "Mount McKinley"
+    ],
+    correctAnswer: "Mount Everest"
+  },
+  {
+    question: "What is the largest desert in the world?",
+    options: ["Sahara Desert", "Gobi Desert", "Atacama Desert", "Antarctica"],
+    correctAnswer: "Sahara Desert"
+  },
+  {
+    question: "Which river is the longest in the world?",
+    options: [
+      "Amazon River",
+      "Nile River",
+      "Mississippi River",
+      "Yangtze River"
+    ],
+    correctAnswer: "Nile River"
+  },
+  {
+    question: "What is the capital of Japan?",
+    options: ["Kyoto", "Osaka", "Tokyo", "Hiroshima"],
+    correctAnswer: "Tokyo"
+  },
+  {
+    question: "In which country can you find the Great Barrier Reef?",
+    options: ["Australia", "Indonesia", "Philippines", "Fiji"],
+    correctAnswer: "Australia"
+  },
+  {
+    question: "Which continent is known as the 'Land Down Under'?",
+    options: ["Africa", "Asia", "Europe", "Australia"],
+    correctAnswer: "Australia"
+  },
+  {
+    question: "What is the largest ocean in the world?",
+    options: [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Arctic Ocean",
+      "Pacific Ocean"
+    ],
+    correctAnswer: "Pacific Ocean"
+  },
+  {
+    question: "Which African country is known as the 'Rainbow Nation'?",
+    options: ["Nigeria", "Kenya", "Egypt", "South Africa"],
+    correctAnswer: "South Africa"
+  },
+  {
+    question: "What is the highest waterfall in the world?",
+    options: ["Victoria Falls", "Angel Falls", "Niagara Falls", "Iguazu Falls"],
+    correctAnswer: "Angel Falls"
+  }
+];
+
+const questionsAnimals = [
+  {
+    question: "What is the largest species of shark?",
+    options: [
+      "Great White Shark",
+      "Hammerhead Shark",
+      "Whale Shark",
+      "Tiger Shark"
+    ],
+    correctAnswer: "Whale Shark"
+  },
+  {
+    question: "What is the largest land animal on Earth?",
+    options: [
+      "African Elephant",
+      "Giraffe",
+      "White Rhinoceros",
+      "Hippopotamus"
+    ],
+    correctAnswer: "African Elephant"
+  },
+  {
+    question: "What is the fastest land animal?",
+    options: ["Cheetah", "Lion", "Leopard", "Gazelle"],
+    correctAnswer: "Cheetah"
+  },
+  {
+    question:
+      "Which bird is known for its beautiful tail feathers and courtship dance?",
+    options: ["Penguin", "Peacock", "Parrot", "Flamingo"],
+    correctAnswer: "Peacock"
+  },
+  {
+    question: "What is the world's smallest mammal?",
+    options: ["Squirrel", "Kangaroo", "Bat", "Hamster"],
+    correctAnswer: "Bat"
+  },
+  {
+    question: "Which animal is known as the 'King of the Jungle'?",
+    options: ["Leopard", "Lion", "Tiger", "Cheetah"],
+    correctAnswer: "Lion"
+  },
+  {
+    question: "What is the largest species of penguin?",
+    options: [
+      "Emperor Penguin",
+      "African Penguin",
+      "King Penguin",
+      "Little Blue Penguin"
+    ],
+    correctAnswer: "Emperor Penguin"
+  },
+  {
+    question:
+      "Which reptile is known for changing the color of its skin to blend with its surroundings?",
+    options: ["Alligator", "Komodo Dragon", "Chameleon", "Iguana"],
+    correctAnswer: "Chameleon"
+  },
+  {
+    question: "What is the national bird of the United States?",
+    options: ["Eagle", "Falcon", "Hawk", "Owl"],
+    correctAnswer: "Eagle"
+  },
+  {
+    question:
+      "Which animal is capable of regenerating lost body parts, including limbs?",
+    options: ["Starfish", "Jellyfish", "Octopus", "Squid"],
+    correctAnswer: "Starfish"
+  }
+];
+
+const questionsSport = [
+  {
+    question: "Which sport is known as the 'Beautiful Game'?",
+    options: ["Basketball", "Tennis", "Soccer", "Golf"],
+    correctAnswer: "Soccer"
+  },
+  {
+    question: "In which country did the sport of tennis originate?",
+    options: ["France", "United States", "England", "Spain"],
+    correctAnswer: "France"
+  },
+  {
+    question:
+      "How many players are there on a standard soccer team during a match?",
+    options: ["9", "10", "11", "12"],
+    correctAnswer: "11"
+  },
+  {
+    question: "Which country is famous for originating the sport of cricket?",
+    options: ["Australia", "India", "United Kingdom", "South Africa"],
+    correctAnswer: "United Kingdom"
+  },
+  {
+    question: "In which sport is the 'Super Bowl' the championship game?",
+    options: ["Baseball", "American Football", "Basketball", "Ice Hockey"],
+    correctAnswer: "American Football"
+  },
+  {
+    question: "What is the highest possible score in a single game of bowling?",
+    options: ["200", "250", "300", "350"],
+    correctAnswer: "300"
+  },
+  {
+    question: "Which sport involves riding waves on a longboard or shortboard?",
+    options: ["Surfing", "Swimming", "Kayaking", "Rowing"],
+    correctAnswer: "Surfing"
+  },
+  {
+    question:
+      "Which athlete is often referred to as 'The Greatest' in the sport of boxing?",
+    options: [
+      "Mike Tyson",
+      "Muhammad Ali",
+      "Floyd Mayweather Jr.",
+      "Manny Pacquiao"
+    ],
+    correctAnswer: "Muhammad Ali"
+  },
+  {
+    question:
+      "In which Olympic event would you see athletes performing the 'Fosbury Flop'?",
+    options: ["High Jump", "Pole Vault", "Long Jump", "Triple Jump"],
+    correctAnswer: "High Jump"
+  },
+  {
+    question:
+      "What is the name of the prestigious golf tournament held annually in Augusta, Georgia?",
+    options: [
+      "The British Open",
+      "The Masters",
+      "The U.S. Open",
+      "The PGA Championship"
+    ],
+    correctAnswer: "The Masters"
+  }
+];
+
+const questionsScience = [
+  {
+    question: "What is the chemical symbol for water?",
+    options: ["H2O", "CO2", "O2", "CH4"],
+    correctAnswer: "H2O"
+  },
+  {
+    question:
+      "Which gas do plants absorb from the atmosphere and use for photosynthesis?",
+    options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Methane"],
+    correctAnswer: "Carbon Dioxide"
+  },
+  {
+    question: "What is the smallest unit of matter?",
+    options: ["Molecule", "Atom", "Proton", "Neutron"],
+    correctAnswer: "Atom"
+  },
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    correctAnswer: "Mars"
+  },
+  {
+    question:
+      "What is the process by which plants make their own food using sunlight?",
+    options: ["Respiration", "Fermentation", "Photosynthesis", "Digestion"],
+    correctAnswer: "Photosynthesis"
+  },
+  {
+    question: "What is the largest organ in the human body?",
+    options: ["Heart", "Liver", "Brain", "Skin"],
+    correctAnswer: "Skin"
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    options: ["Ag", "Au", "Fe", "Hg"],
+    correctAnswer: "Au"
+  },
+  {
+    question:
+      "Which gas do humans breathe out as a waste product of respiration?",
+    options: ["Carbon Dioxide", "Oxygen", "Nitrogen", "Argon"],
+    correctAnswer: "Carbon Dioxide"
+  },
+  {
+    question:
+      "What is the process by which an organism develops from a fertilized egg?",
+    options: ["Metamorphosis", "Evolution", "Reproduction", "Development"],
+    correctAnswer: "Development"
+  },
+  {
+    question:
+      "What is the center of an atom called, which contains protons and neutrons?",
+    options: ["Nucleus", "Cell", "Electron Cloud", "Molecule"],
+    correctAnswer: "Nucleus"
+  }
+];
+
+const questionsHistory = [
+  {
+    question: "Who was the first President of the United States?",
+    options: [
+      "Thomas Jefferson",
+      "John Adams",
+      "George Washington",
+      "Benjamin Franklin"
+    ],
+    correctAnswer: "George Washington"
+  },
+  {
+    question:
+      "In which year did Christopher Columbus first arrive in the Americas?",
+    options: ["1492", "1520", "1607", "1776"],
+    correctAnswer: "1492"
+  },
+  {
+    question: "Which ancient civilization built the Great Pyramids of Giza?",
+    options: [
+      "Roman Empire",
+      "Maya Civilization",
+      "Egyptian Civilization",
+      "Greek Empire"
+    ],
+    correctAnswer: "Egyptian Civilization"
+  },
+  {
+    question: "During which war was the Battle of Gettysburg fought?",
+    options: [
+      "American Revolution",
+      "Civil War",
+      "World War I",
+      "World War II"
+    ],
+    correctAnswer: "Civil War"
+  },
+  {
+    question:
+      "Who wrote the 'Declaration of Independence' for the United States?",
+    options: [
+      "George Washington",
+      "Benjamin Franklin",
+      "John Adams",
+      "Thomas Jefferson"
+    ],
+    correctAnswer: "Thomas Jefferson"
+  },
+  {
+    question:
+      "Which famous ancient wonder of the world was located in Alexandria, Egypt?",
+    options: [
+      "The Hanging Gardens of Babylon",
+      "The Lighthouse of Alexandria",
+      "The Great Wall of China",
+      "The Colossus of Rhodes"
+    ],
+    correctAnswer: "The Lighthouse of Alexandria"
+  },
+  {
+    question: "What event marked the start of World War I?",
+    options: [
+      "The assassination of Archduke Franz Ferdinand",
+      "The sinking of the Titanic",
+      "The signing of the Treaty of Versailles",
+      "The Boston Tea Party"
+    ],
+    correctAnswer: "The assassination of Archduke Franz Ferdinand"
+  },
+  {
+    question: "Who was the first woman to fly solo across the Atlantic Ocean?",
+    options: [
+      "Amelia Earhart",
+      "Bessie Coleman",
+      "Sally Ride",
+      "Harriet Quimby"
+    ],
+    correctAnswer: "Amelia Earhart"
+  },
+  {
+    question: "Which U.S. President signed the Emancipation Proclamation?",
+    options: [
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "George Washington",
+      "Andrew Jackson"
+    ],
+    correctAnswer: "Abraham Lincoln"
+  },
+  {
+    question:
+      "In which year did the Berlin Wall fall, leading to the reunification of Germany?",
+    options: ["1961", "1989", "1991", "2001"],
+    correctAnswer: "1989"
+  }
+];
+
+let currentQuestion = 0;
+let score = 0; //variable to keep my score
+
+const questionText = document.getElementById("question-text");
+const optionButtons = document.querySelectorAll(".option-button");
+const nextButton = document.getElementById("next-button");
+const scoreDisplay = document.getElementById("score-display");
+
+function startQuiz() {
+  const categoryButtons = document.querySelectorAll(".btn-category");
+
+  for (let i = 0; i < categoryButtons.length; i++) {
+    categoryButtons[i].addEventListener("click", () =>
+      chooseCategory(categoryButtons[i].textContent)
+    );
+  }
+}
+
+function chooseCategory(categoryName) {
+  switch (categoryName) {
+    case "Geography":
+      loadCategory(categoryName);
+      showQuestion(questionsGeography, currentQuestion);
+      nextButtonClicked(questionsGeography);
+      break;
+    case "Animals":
+      loadCategory(categoryName);
+      showQuestion(questionsAnimals, currentQuestion);
+      nextButtonClicked(questionsAnimals);
+      break;
+    case "Sport":
+      loadCategory(categoryName);
+      showQuestion(questionsSport, currentQuestion);
+      nextButtonClicked(questionsSport);
+      break;
+    case "Science":
+      loadCategory(categoryName);
+      showQuestion(questionsScience, currentQuestion);
+      nextButtonClicked(questionsScience);
+      break;
+    case "History":
+      loadCategory(categoryName);
+      showQuestion(questionsHistory, currentQuestion);
+      nextButtonClicked(questionsHistory);
+      break;
+    default:
+      alert("Please select the correct category");
+  }
+}
+
+function loadCategory(categoryName) {
+  const categorySection = document.getElementById("section-categories");
+  const questionSection = document.getElementById("section-questions");
+
+  categorySection.style.display = "none";
+  questionSection.style.visibility = "visible";
+  document.getElementById("category-name").textContent = categoryName;
+}
+
+function showQuestion(questions, questionIndex) {
+  const question = questions[questionIndex];
+  questionText.textContent = question.question;
+
+  for (let i = 0; i < optionButtons.length; i++) {
+    optionButtons[i].textContent = question.options[i];
+    if (optionButtons[i].textContent === question.correctAnswer) {
+      addClassesToCorrectOption(optionButtons[i]);
+    }
+
+    optionButtons[i].addEventListener("click", () => {
+      checkAnswer(questions, questionIndex, optionButtons[i]);
+    });
+  }
+}
+
+function addClassesToCorrectOption(target) {
+  optionButtons.forEach((btn) => {
+    if (btn == target) {
+      btn.classList.add("correct");
+    } else {
+      btn.classList.remove("correct");
+    }
+  });
+}
+let answered = false; // Add this flag
+
+function checkAnswer(questions, questionIndex, selectedOption) {
+  if (answered) return; // Check if the question has already been answered
+
+  const question = questions[questionIndex];
+  if (selectedOption.classList.contains("correct")) {
+    // Handle correct answer
+    selectedOption.style.backgroundColor = "#23bd1e";
+    selectedOption.style.border = "2px solid #23bd1e";
+    selectedOption.style.color = "#fff";
+    score++; // increase the result
+  } else {
+    // Handle wrong answer
+    selectedOption.style.backgroundColor = "#ff0051";
+    selectedOption.style.border = "2px solid #ff0051";
+    selectedOption.style.color = "#fff";
+
+    // Highlight the correct answer
+    optionButtons.forEach((btn) => {
+      if (btn.classList.contains("correct")) {
+        btn.style.backgroundColor = "#23bd1e";
+        btn.style.border = "2px solid #23bd1e";
+        btn.style.color = "#fff";
+      }
+    });
+  }
+
+  // Update the score display regardless of the answer
+  scoreDisplay.textContent = `Score: ${score}`;
+  answered = true; // Set the flag to true
+
+
+  // Show the "Next" button
+  nextButton.style.display = "block";
+
+  // Disable option buttons after selecting an answer
+  optionButtons.forEach((button) => (button.disabled = true));
+}
+
+function nextButtonClicked(questions) {
+  nextButton.addEventListener("click", () => {
+    currentQuestion++;
+    answered = false; //Reset the answered flag
+
+    if (currentQuestion < questions.length) {
+      // Clear the button background color and enable option buttons
+      optionButtons.forEach((button) => {
+        button.style.backgroundColor = "#fff";
+        button.style.color = "#57009e";
+        button.style.border = "2px solid #57009e";
+        button.disabled = false;
+      });
+      // Hide the "Next" button
+      nextButton.style.display = "none";
+      showQuestion(questions, currentQuestion);
+    } else {
+      // Quiz is finished
+      questionText.textContent = "Quiz Completed!";
+      optionButtons.forEach((button) => (button.style.display = "none"));
+      nextButton.style.display = "none";
+      scoreDisplay.textContent = `Final Score: ${score}`; // display the result
+    }
+  });
+}
+
+startQuiz();
